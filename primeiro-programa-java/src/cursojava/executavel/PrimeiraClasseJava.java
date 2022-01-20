2a2fce841d6c9959f8958af235e8033e178c80bb
Francisco Luiz Tavares Ramos <49736639+fltramos@users.noreply.github.com>
2022-01-20 20:59:25 -0300

    Aula sobre operadores lógicos

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -3,7 +3,32 @@ package cursojava.executavel;
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-		System.out.println("Meu primeiro programa em Java!!!");
-		System.out.println("Usando Git no Eclipse!");
+		System.out.println("Operadores Lógicos");
+
+		/* Operadores Lógocos com IF e Else */
+
+		int nota1 = 90;
+		int nota2 = 80;
+		int nota3 = 70;
+		int nota4 = 5;
+		int media = 0;
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		/* Condições lógicas com IF e Else */
+
+		if (media >= 70) {
+			System.out.println("Aluno aprovado: " + media);
+		} else if (media >= 40 && media <= 69) {
+			System.out.println("Aluno em recuperação: " + media);
+		} else {
+			System.out.println("Aluno reprovado: " + media);
+		}
+
+		/* Operadores ternários para micro válidações em uma linha- neste caso o IF é representado pela ? e o Else por : */
+
+		String saidaResultado = media >= 70 ? "Aluno Aprovado!" : (media >= 40 && media <= 69) ? "Aluno em recuperação" : "Aluno Reprovado!?";
+		System.out.println(saidaResultado);
+
 	}
 }
