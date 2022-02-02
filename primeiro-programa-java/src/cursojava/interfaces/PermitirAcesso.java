1ddbf382563ccad344faab04824737ba44db7fea
Francisco Luiz Tavares Ramos <49736639+fltramos@users.noreply.github.com>
2022-02-01 21:54:40 -0300

    Retirada de comentario

--- primeiro-programa-java/src/cursojava/interfaces/PermitirAcesso.java
+++ primeiro-programa-java/src/cursojava/interfaces/PermitirAcesso.java
@@ -2,7 +2,7 @@ package cursojava.interfaces;
 
 public interface PermitirAcesso {
 	
-	public boolean autenticar(String login, String senha);//Apenas declaração do método.
+	public boolean autenticar(String login, String senha);
 	
 	public boolean autenticar();//Apenas declaração do método.
 
