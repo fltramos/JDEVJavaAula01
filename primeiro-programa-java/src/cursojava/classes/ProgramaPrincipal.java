9ac3ca484e3f11b0c04407cb8b8e86b90249da4a
Francisco Luiz Tavares Ramos <49736639+fltramos@users.noreply.github.com>
2022-02-07 00:27:07 -0300

    Testando Array
    Alterando a classe disciplina para Array
    
    Adicionado o metodo getMediaNotas na classe Disciplina
    
    Altarado o metodo getMediaNota na classe Aluno
    
    Testando Array

--- primeiro-programa-java/src/cursojava/classes/ProgramaPrincipal.java
+++ primeiro-programa-java/src/cursojava/classes/ProgramaPrincipal.java
@@ -90,7 +90,7 @@ public class ProgramaPrincipal {
 
 					Disciplina disciplina = new Disciplina();
 					disciplina.setDisciplina(nomeDisciplina);
-					disciplina.setNota(Double.valueOf(notaDisciplina));
+					//disciplina.setNota(Double.valueOf(notaDisciplina));
 
 					aluno1.getDisciplinas().add(disciplina);
 				}
--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -2,17 +2,93 @@ package cursojava.executavel;
 
 import javax.swing.JOptionPane;
 
+import cursojava.classes.Aluno;
+import cursojava.classes.Disciplina;
+
 public class ArrayVetor {
 
 	public static void main(String[] args) {
 		
+		Aluno aluno = new Aluno();
+		aluno.setNome("Francisco");
+		aluno.setNomeEscola("JDev");
+		
+		Disciplina disciplina = new Disciplina();
+		disciplina.setDisciplina("Java");		
+		double[] notas = {8.8, 9.7, 7.6, 6.8};
+		disciplina.setNota(notas);
+		
+		aluno.getDisciplinas().add(disciplina);
+		
+		Disciplina disciplina2 = new Disciplina();
+		disciplina2.setDisciplina("Python");		
+		double[] notas2 = {9.8, 8.9, 9.9, 9.8};		
+		disciplina2.setNota(notas2);
+		
+		aluno.getDisciplinas().add(disciplina2);
+		
+		Aluno[] arrayAlunos = new Aluno[1];
+		
+		arrayAlunos[0] = aluno;
+		
+		for (int pos = 0; pos < arrayAlunos.length; pos ++) {
+			System.out.println("Nome do aluno: " + arrayAlunos[pos].getNome());
+			
+			for (Disciplina d:arrayAlunos[pos].getDisciplinas()) {
+				System.out.println("Nome da disciplina é: " + d.getDisciplina());
+				
+				for (int posNota = 0; posNota < d.getNota().length; posNota ++) {
+					System.out.println("A nota " + (posNota+1) + " é: " + d.getNota()[posNota]);
+				}
+				System.out.println("A média final foi: " + d.getMediaNotas());
+			}
+
+				
+		}
+		
+		/*System.out.println("Nome do Aluno: " + aluno.getNome() + ", inscrito no curso: " + aluno.getNomeEscola());
+		
+		for (Disciplina d : aluno.getDisciplinas()) {
+			System.out.println("-------------------- Disciplinas do Aluno --------------------");
+			System.out.println("Disciplina: " + d.getDisciplina());
+			System.out.println("As notas da disciplina " + d.getDisciplina() + " são: ");
+			
+			double notaMax = 0.0;
+			double notaMin = 0.0;
+			
+			for (int pos = 0; pos < d.getNota().length; pos ++) {
+				System.out.println("Nota " + (pos+1) + " é igual: " + d.getNota()[pos]);
+				
+				if (pos == 0) {
+					notaMax = d.getNota()[pos];
+				}else
+					if (d.getNota()[pos] > notaMax) {
+						notaMax = d.getNota()[pos];
+					}
+				
+				if (pos == 0) {
+					notaMin = d.getNota()[pos];
+				}else
+					if (d.getNota()[pos] < notaMin) {
+						notaMin = d.getNota()[pos];
+					}
+											
+			}
+			System.out.println("A média final foi: " + d.getMediaNotas());
+			System.out.println("A maior nota da Disciplina " + d.getDisciplina() + " foi :" + notaMax);
+			System.out.println("A menor nota da Disciplina " + d.getDisciplina() + " foi :" + notaMin);
+		}*/
+		
+	}
+		
 		//Criando um Array dinamicamente
 		
-		String posicoes = JOptionPane.showInputDialog("Quantas posições o Array deve ter? ");
+		/*String posicoes = JOptionPane.showInputDialog("Quantas posições o Array deve ter? ");
 		
 		
 		//double notas[] = new double[4]; O array pode ser declarado assim ou:
 		double[] notas = new double[Integer.parseInt(posicoes)];
+		String[] dados = {"Francisco", "Casa", "Telefone"};//Declarando e criando o array.
 		
 		for (int pos = 0; pos < notas.length; pos ++) {
 			String valor = JOptionPane.showInputDialog("Qual o valor da posição = " + pos);
@@ -24,9 +100,9 @@ public class ArrayVetor {
 		notas[2] = 9.1;
 		notas[3] = 7.6;*/
 		
-		for (int pos = 0; pos < notas.length; pos ++) {
+		/*for (int pos = 0; pos < notas.length; pos ++) {
 			System.out.println("Nota " + (pos + 1) + " é = " + notas[pos]); //Começa da posição 0 mas imprime 1
 		}
 		
-	}
+	}*/
 }
